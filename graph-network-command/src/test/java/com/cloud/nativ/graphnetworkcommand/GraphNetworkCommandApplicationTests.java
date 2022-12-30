dcd7a16478ab7589eb72073f90d6e91a55d65ac7
lsefiane <lyes.sefiane@gmail.com>
2022-12-30 12:06:13 -0500

    M.E.R.I.T : Graph Network Command

---
+++ graph-network-command/src/test/java/com/cloud/nativ/graphnetworkcommand/GraphNetworkCommandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.nativ.graphnetworkcommand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphNetworkCommandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
