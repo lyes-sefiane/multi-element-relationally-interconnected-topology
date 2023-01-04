7d2fe184894339c63bacc18b26e4dc87df07459c
lsefiane <lyes.sefiane@gmail.com>
2023-01-04 14:10:10 -0500

    M.E.R.I.T : Network Elements RESTful Web Service

---
+++ network-elements/src/main/java/com/cloud/nativ/networkelements/converter/ModelMapper.java
@@ -0,0 +1,78 @@
+package com.cloud.nativ.networkelements.converter;
+
+import com.cloud.nativ.networkelements.controller.NetworkDeviceController;
+import com.cloud.nativ.networkelements.domain.entities.Connection;
+import com.cloud.nativ.networkelements.domain.entities.Neighbor;
+import com.cloud.nativ.networkelements.domain.entities.NetworkDevice;
+import com.cloud.nativ.networkelements.domain.enums.ElementType;
+import com.cloud.nativ.networkelements.dto.entities.NeighborDto;
+import com.cloud.nativ.networkelements.dto.entities.NetworkDeviceDto;
+import org.springframework.hateoas.Link;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+/**
+ * @author : Lyes Sefiane
+ * @mailto : lyes.sefiane@gmail.com
+ * @created : 2021-12-25 12:17 p.m.
+ */
+@Component
+public class ModelMapper implements IModelMapper<NetworkDeviceDto, NetworkDevice> {
+
+    /**
+     * Convert DTO to Entity
+     *
+     * @param networkDeviceDto
+     * @return networkDevice
+     */
+    @Override
+    public NetworkDevice toEntity(NetworkDeviceDto networkDeviceDto) {
+
+        NetworkDevice networkDevice = NetworkDevice.builder()//
+                .ipAddress(networkDeviceDto.getAddress())//
+                .elementType(ElementType.getEnumByValue(networkDeviceDto.getElementType()))//
+                .build();
+
+        Set<Connection> connections = networkDeviceDto.getNeighbors()//
+                .stream()//
+                .map(neighborDto -> new Connection(networkDevice, new Neighbor(neighborDto.getAddress()), neighborDto.getCost()))//
+                .collect(Collectors.toSet());
+
+        networkDevice.setConnections(connections);
+
+        return networkDevice;
+    }
+
+    /**
+     * Convert Entity to DTO
+     *
+     * @param networkDevice
+     * @return networkDeviceDto
+     */
+    @Override
+    public NetworkDeviceDto toDto(NetworkDevice networkDevice) {
+
+        NetworkDeviceDto networkDeviceDto = NetworkDeviceDto.builder()//
+                .address(networkDevice.getIpAddress())//
+                .elementType(networkDevice.getElementType().getValue())//
+                .build();
+
+        Set<NeighborDto> neighbors = networkDevice.getConnections()//
+                .stream()//
+                .map(connection -> new NeighborDto(connection.getConnectionId().getNeighbor_ipaddress(), connection.getCost()))//
+                .collect(Collectors.toSet());
+
+        networkDeviceDto.setNeighbors(neighbors);
+
+        Link link = linkTo(methodOn(NetworkDeviceController.class).findById(networkDeviceDto.getAddress())).withSelfRel();
+
+        networkDeviceDto.add(link);
+
+        return networkDeviceDto;
+    }
+}
---
+++ network-elements/src/test/java/com/cloud/nativ/networkelements/NetworkElementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.nativ.networkelements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkElementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
