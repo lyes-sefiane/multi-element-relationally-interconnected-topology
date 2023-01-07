387145eec5ae4947fce41308874baf31290f64e5
lsefiane <lyes.sefiane@gmail.com>
2023-01-07 14:30:57 -0500

    M.E.R.I.T : Network Common

--- network-common/src/main/java/com.cloud.nativ.networkcommon/NetworkCommonApplication.java
+++ network-common/src/main/java/com.cloud.nativ.networkcommon/NetworkCommonApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.cloud.nativ.networkcommon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
