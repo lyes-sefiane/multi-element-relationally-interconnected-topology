fac858b0d885ccf74e0fbc9dc74574ecf6568a7b
lsefiane <lyes.sefiane@gmail.com>
2023-01-07 14:10:57 -0500

    M.E.R.I.T : Network Graph

--- network-graph/src/main/java/com/cloud/nativ/networkgraph/NetworkGraphApplication.java
+++ network-graph/src/main/java/com/cloud/nativ/networkgraph/NetworkGraphApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.cloud.nativ.networkgraph;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
