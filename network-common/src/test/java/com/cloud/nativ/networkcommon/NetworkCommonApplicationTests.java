6c1ae6c8311648699f9b2eb0e20b8d472055c660
lsefiane <lyes.sefiane@gmail.com>
2023-01-09 22:39:44 -0500

    M.E.R.I.T : Network Common
    - Avro Schema definition and entities generation centralization.
    - Added as dependence in different modules.

---
+++ network-common/src/test/java/com/cloud/nativ/networkcommon/NetworkCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.nativ.networkcommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
