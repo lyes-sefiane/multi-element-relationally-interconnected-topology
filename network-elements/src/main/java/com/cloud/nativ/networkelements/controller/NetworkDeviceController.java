c9fb8b0ae72432e76dc72f8e972b72b2494563a9
lsefiane <lyes.sefiane@gmail.com>
2023-01-13 19:18:22 -0500

    M.E.R.I.T : Network Common Update

--- network-elements/src/main/java/com/cloud/nativ/networkelements/controller/NetworkDeviceController.java
+++ network-elements/src/main/java/com/cloud/nativ/networkelements/controller/NetworkDeviceController.java
@@ -1,29 +1,40 @@
 package com.cloud.nativ.networkelements.controller;
 
-import com.cloud.nativ.networkelements.dto.entities.NetworkDeviceDto;
-import com.cloud.nativ.networkcommon.exception.entities.ExceptionResponse;
-import com.cloud.nativ.networkelements.service.IService;
-import com.cloud.nativ.networkelements.utils.Defines;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Pattern;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.ExposesResourceFor;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-import javax.validation.constraints.Pattern;
-import java.util.List;
-import java.util.stream.Collectors;
+import com.cloud.nativ.networkelements.dto.entities.NetworkDeviceDto;
+import com.cloud.nativ.networkelements.exception.entities.ExceptionResponse;
+import com.cloud.nativ.networkelements.service.IService;
+import com.cloud.nativ.networkelements.utils.Defines;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 /**
  * @author : Lyes Sefiane
