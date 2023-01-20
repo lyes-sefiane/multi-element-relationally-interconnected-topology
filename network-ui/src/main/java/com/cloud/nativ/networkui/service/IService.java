2cb432f95e12de6f3e38ecefba7c49cbd32ec0c7
lsefiane <lyes.sefiane@gmail.com>
2023-01-19 19:15:51 -0500

    M.E.R.I.T : Network UI
    - Service Definition

---
+++ network-ui/src/main/java/com/cloud/nativ/networkui/service/IService.java
@@ -0,0 +1,15 @@
+package com.cloud.nativ.networkui.service;
+
+import com.cloud.nativ.networkui.domain.NetworkElement;
+
+import java.util.List;
+
+/**
+ * @author : Lyes Sefiane
+ * @mailto : lyes.sefiane@gmail.com
+ * @created : 2023-01-19 6:53 p.m.
+ */
+public interface IService {
+
+    List<NetworkElement> getAllNetworkElements();
+}
