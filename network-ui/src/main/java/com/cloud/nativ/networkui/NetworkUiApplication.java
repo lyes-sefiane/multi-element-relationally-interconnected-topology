e325faaa17107510bad37f7df18614021e59aa7c
lsefiane <lyes.sefiane@gmail.com>
2023-01-21 13:09:57 -0500

    M.E.R.I.T : User Interface Update
    - Enabled Application Reactive Mode

--- network-ui/src/main/java/com/cloud/nativ/networkui/NetworkUiApplication.java
+++ network-ui/src/main/java/com/cloud/nativ/networkui/NetworkUiApplication.java
@@ -1,13 +1,16 @@
 package com.cloud.nativ.networkui;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NetworkUiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NetworkUiApplication.class, args);
+		SpringApplication springApplication = new SpringApplication(NetworkUiApplication.class);
+		springApplication.setWebApplicationType(WebApplicationType.REACTIVE);
+		springApplication.run(args);
 	}
 
 }
